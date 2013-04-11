8ee2dbcb24dcb6ecdd7fe58ea91b140c724c0e13
Tucker Joseph <rtjoseph11@gmail.com>
2013-04-10 20:55:15 -0700

    implemented stringifyJSON and turned off the tests for parseJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,48 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  var returnValue;
+  switch(typeof obj){
+  	case "string":
+  		returnValue = '"' + obj + '"';
+  		break;
+  	case "number":
+  		returnValue = obj.toString();
+  		break;
+  	case "undefined":
+  		returnValue = obj;
+  		break;
+  	case "boolean":
+  		returnValue = obj.toString();
+  		break;
+  	case "object":
+  		switch(true){
+  			case !Boolean(obj):
+  				returnValue = 'null';
+  				break;
+  			case Array.isArray(obj):
+  				returnValue = '[';
+  				for (var i = 0; i < obj.length; i++){
+  					if (i > 0){
+  						returnValue += ',';
+  					}
+  					returnValue += stringifyJSON(obj[i]);
+  				}
+  				returnValue += ']';
+  				break;
+  			default:
+  				returnValue = '{';
+  				for (var item in obj){
+  					if (typeof obj[item] != 'function' && typeof obj[item] != 'undefined' && typeof item != 'undefined'){
+  						if (item !== Object.keys(obj)[0]){
+  							returnValue += ',';
+  						}
+  						returnValue += '"' + item + '":' + stringifyJSON(obj[item]);
+  					}
+  				}
+  				returnValue += '}';
+  		}
+  		break;
+  }
+  return returnValue;
 };
